f3f5cb1e3159cace310b025c1cb885977ce23a68
EC2 Default User <ec2-user@ip-172-31-32-163.ap-south-1.compute.internal>
2026-04-01 07:16:17 +0000

    updated response message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from GitHub Actions 🚀");
+  res.end("Updated via GitHub Actions 🚀🔥");
 });
 
 server.listen(3000, () => {
